1299dac869b9f1d8a9e2c75c7ffc64b5133206cb
Myersonk <kyle_myerson@yahoo.com>
2014-01-18 20:26:36 -0500

    First commit

---
+++ ToBeNamed/src/ri/game/Game.java
@@ -0,0 +1,20 @@
+package ri.game;
+
+public class Game {
+
+	
+	
+	public Game(){
+		
+	}
+	
+	public void start(){
+		
+	}
+	
+	
+	public static void Main(String[] args){
+		Game game = new Game();
+		game.start();
+	}
+}
