aabd4f0b8b75aed1dede9027d7007fb611f9b028
Myersonk <kyle_myerson@yahoo.com>
2014-01-18 20:33:23 -0500

    added stuff to time class

--- ToBeNamed/src/ri/game/Time.java
+++ ToBeNamed/src/ri/game/Time.java
@@ -3,8 +3,27 @@ package ri.game;
 public class Time {
 	
 	
-	public static double getTime(){
+	private static double delta;
+	public static final long SECOND = 1000000000L;
+	
+	
+	public static long getTimeNano(){
 		return System.nanoTime();
+		
 	}
+	
+	public static double getDelta(){
+		return delta;
+	}
+	
+	public static void setDelta(double d){
+		delta = d;
+		
+	}
+	public static double getTimeSec(){
+		return ((double)Time.getTimeNano())/((double)Time.SECOND);
+	
+	}
+
 
 }
