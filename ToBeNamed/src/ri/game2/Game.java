fe17950f5c292080829f23b62577003f9c8c71cd
Myersonk <kyle_myerson@yahoo.com>
2014-01-18 21:45:51 -0500

    why do i have to write a message

--- ToBeNamed/src/ri/game2/Game.java
+++ ToBeNamed/src/ri/game2/Game.java
@@ -16,8 +16,8 @@ public class Game extends Canvas implements Runnable {
 
 	private static final long serialVersionUID = 1L;
 
-	public static final int WIDTH = 320;
-	public static final int HEIGHT = WIDTH / 12 * 9;
+	public static final int WIDTH = 400;
+	public static final int HEIGHT = WIDTH / 4 * 3;
 	public static final int SCALE = 2;
 	public static final String NAME = "Game";
 	public static final Dimension DIMENSIONS = new Dimension(WIDTH * SCALE,HEIGHT * SCALE);
